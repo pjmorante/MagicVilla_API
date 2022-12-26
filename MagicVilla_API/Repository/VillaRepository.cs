e846d0e9bfa08d133bff9252ed4d87eaa1ba9e66
Pedro Morante <pedro.morante@outlook.com>
2022-12-26 09:10:38 -0500

    Add Repository Interface

---
+++ MagicVilla_API/Repository/VillaRepository.cs
@@ -0,0 +1,60 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public VillaRepository(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public async Task Create(Villa entity)
+        {
+            await _db.Villa.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _db.Villa;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            IQueryable<Villa> query = _db.Villa;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villa.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
