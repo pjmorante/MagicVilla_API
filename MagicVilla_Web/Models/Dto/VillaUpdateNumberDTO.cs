931f2891c7ad7009a16fecd1982b614567042d1f
Pedro Morante <pedro.morante@outlook.com>
2022-12-27 09:22:14 -0500

    Add web project's DTO and API models

---
+++ MagicVilla_Web/Models/Dto/VillaUpdateNumberDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.Dto
+{
+    public class VillaUpdateNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        [Required]
+        public int VillaID { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
