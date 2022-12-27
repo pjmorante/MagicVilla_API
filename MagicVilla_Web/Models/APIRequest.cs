2588bdc3c8466bb1ad6508f6121ef6917378b585
Pedro Morante <pedro.morante@outlook.com>
2022-12-27 09:34:29 -0500

    Add AutoMapper to web project

--- MagicVilla_Web/Models/APIRequest.cs
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -5,5 +5,8 @@ namespace MagicVilla_Web.Models
     public class APIRequest
     {
         public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+
     }
 }
