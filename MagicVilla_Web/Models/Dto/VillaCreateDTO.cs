289b846dc7e6d84e2c99a49f30200df9c30f7fd9
Pedro Morante <pedro.morante@outlook.com>
2022-12-28 15:34:29 -0500

    Add Delete Villa Controller

--- MagicVilla_Web/Models/Dto/VillaCreateDTO.cs
+++ MagicVilla_Web/Models/Dto/VillaCreateDTO.cs
@@ -12,7 +12,7 @@ namespace MagicVilla_Web.Models.Dto
         public Double Rate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
-        public string ImageUrl { get; set; }
-        public string Amenity { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Amenity { get; set; }
     }
 }
--- MagicVilla_Web/Services/VillaService.cs
+++ MagicVilla_Web/Services/VillaService.cs
@@ -21,7 +21,7 @@ namespace MagicVilla_Web.Services
             {
                 ApiType = SD.ApiType.POST,
                 Data = dto,
-                Url = villaUrl + "/api/villaAPI"
+                Url = villaUrl + "/api/VillaAPI"
             });
         }
 
@@ -30,7 +30,7 @@ namespace MagicVilla_Web.Services
             return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.DELETE,
-                Url = villaUrl + "/api/villaAPI/" + id
+                Url = villaUrl + "/api/VillaAPI/" + id
             });
         }
 
@@ -39,7 +39,7 @@ namespace MagicVilla_Web.Services
             return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = villaUrl + "/api/villaAPI"
+                Url = villaUrl + "/api/VillaAPI"
             });
         }
 
@@ -48,7 +48,7 @@ namespace MagicVilla_Web.Services
             return SendAsync<T>(new APIRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = villaUrl + "/api/villaAPI/" + id
+                Url = villaUrl + "/api/VillaAPI/" + id
             });
         }
 
@@ -58,7 +58,7 @@ namespace MagicVilla_Web.Services
             {
                 ApiType = SD.ApiType.PUT,
                 Data = dto,
-                Url = villaUrl + "/api/villaAPI/" + dto.Id
+                Url = villaUrl + "/api/VillaAPI/" + dto.Id
             });
         }
     }
