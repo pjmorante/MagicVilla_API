de94f332cb5cdff66e18f628156d755e1d8daf3c
Pedro Morante <pedro.morante@outlook.com>
2022-12-29 10:53:12 -0500

    Add Villa Number Service

---
+++ MagicVilla_Web/Services/IServices/IVillaNumberService.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IVillaNumberService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int id);
+        Task<T> CreateAsync<T>(VillaCreateNumberDTO dto);
+        Task<T> UpdateAsync<T>(VillaUpdateNumberDTO dto);
+        Task<T> DeleteAsync<T>(int id);
+    }
+}
