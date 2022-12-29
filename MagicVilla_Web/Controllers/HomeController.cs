d656dff69808cd8999cbb0074570b4e5a5e1a795
Pedro Morante <pedro.morante@outlook.com>
2022-12-29 14:40:43 -0500

    Add villa home page

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -30,16 +30,5 @@ namespace MagicVilla_Web.Controllers
             }
             return View(list);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
