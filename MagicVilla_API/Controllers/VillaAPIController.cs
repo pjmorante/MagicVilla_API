49169b145a68b7c6e01e6e2f565908136a707baa
Pedro Morante <pedro.morante@outlook.com>
2022-12-30 10:03:13 -0500

    Display API error messages

--- MagicVilla_API/Controllers/VillaAPIController.cs
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -91,7 +91,7 @@ namespace MagicVilla_API.Controllers
             {
                 if (await _dbVilla.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa name already exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa name already exists!");
                     return BadRequest(ModelState);
                 }
                 if (createDTO == null)
--- MagicVilla_API/Controllers/VillaNumberAPIController.cs
+++ MagicVilla_API/Controllers/VillaNumberAPIController.cs
@@ -89,13 +89,13 @@ namespace MagicVilla_API.Controllers
             {
                 if (await _dbVillaNumber.GetAsync(u => u.VillaNo == createDTO.VillaNo) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa Number already exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa Number already exists!");
                     return BadRequest(ModelState);
                 }
 
                 if (await _dbVilla.GetAsync(u => u.Id == createDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is invalid!");
                     return BadRequest(ModelState);
                 }
 
@@ -169,7 +169,7 @@ namespace MagicVilla_API.Controllers
 
                 if (await _dbVilla.GetAsync(u => u.Id == updateDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is invalid!");
                     return BadRequest(ModelState);
                 }
 
