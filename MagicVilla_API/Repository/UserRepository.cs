250f23b81ff8e6669173d353567f6db36050a9dc
Pedro Morante <pedro.morante@outlook.com>
2023-01-16 13:12:21 -0500

    Add JWT Token Login

--- MagicVilla_API/Repository/UserRepository.cs
+++ MagicVilla_API/Repository/UserRepository.cs
@@ -4,6 +4,11 @@ using MagicVilla_API.Models;
 using MagicVilla_API.Models.Dto;
 using MagicVilla_API.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MagicVilla_API.Repository
 {
@@ -11,32 +16,79 @@ namespace MagicVilla_API.Repository
     {
         private readonly ApplicationDBContext _db;
         //private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly RoleManager<IdentityRole> _roleManager;
         private string secretKey;
         private readonly IMapper _mapper;
 
-        public UserRepository(ApplicationDBContext db, IConfiguration configuration,
-            UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
+        public UserRepository(ApplicationDBContext db, IConfiguration configuration, IMapper mapper)
+            //UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
-            _mapper = mapper;
+            //_mapper = mapper;
             //_userManager = userManager;
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
-            _roleManager = roleManager;
+            //_roleManager = roleManager;
         }
         public bool IsUniqueUser(string username)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers
+                .FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+                && u.Password == loginRequestDTO.Password);
+            if(user == null)
+            {
+                return null;
+            }
+            //if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+
+            };
+            return loginResponseDTO;
         }
 
-        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
         {
-            throw new NotImplementedException();
+            LocalUser user = new()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                //Email = registerationRequestDTO.UserName,
+                //NormalizedEmail = registerationRequestDTO.UserName.ToUpper(),
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
         }
     }
 }
