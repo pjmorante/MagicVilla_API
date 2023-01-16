9f51417605bc7eb3cf8889aaeb4928b37fd351de
Pedro Morante <pedro.morante@outlook.com>
2023-01-16 12:05:50 -0500

    Add User Repository

---
+++ MagicVilla_API/Repository/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+
+namespace MagicVilla_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDBContext _db;
+        //private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        private readonly IMapper _mapper;
+
+        public UserRepository(ApplicationDBContext db, IConfiguration configuration,
+            UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _mapper = mapper;
+            //_userManager = userManager;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _roleManager = roleManager;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
